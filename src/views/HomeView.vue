fb27abbd863f04a0c3aad8a2cf55a741db463265
Igor Borges <igorborgeswrk2@gmail.com>
2022-04-28 01:48:17 -0300

    [If applied] will install leaflet and build the map implementation with the public key.

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-col h-screen max-h-screen">
     <!-- Search / Results -->
-    <div class="flex justify-center relative 
+    <div class="z-20 flex justify-center relative 
     bg-hero-pattern bg-cover 
     px-4 pt-8 pb-32">
     <!-- Search Input -->
@@ -19,17 +19,38 @@
     <!-- IP Info -->
     <IPInfo />
     </div>
+
+    <!-- Map -->
+    <div id="map" class="h-full z-10"></div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import IPInfo from "../components/IPInfo.vue"
+// eslint-disable-next-line
+import leaflet from "leaflet"
+import { onMounted } from '@vue/runtime-core';
 
 export default {
   name: 'HomeView',
-  components: {
-    IPInfo
-  }
+  components: { IPInfo },
+  setup () {
+    let mymap;
+
+    onMounted(() => {
+      mymap = leaflet.map('map').setView([51.505, -0.09], 13);
+
+    leaflet.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoiaWdvcmJvcmdlc3MiLCJhIjoiY2wyaWlmNmx2MDNsdzNjb2piZWI5c2IxayJ9.6iNkAR6kZGhg3vPB2tq2fw', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'pk.eyJ1IjoiaWdvcmJvcmdlc3MiLCJhIjoiY2wyaWlmNmx2MDNsdzNjb2piZWI5c2IxayJ9.6iNkAR6kZGhg3vPB2tq2fw'
+  }).addTo(mymap);
+    })
+    
+  },
 }
 </script>
